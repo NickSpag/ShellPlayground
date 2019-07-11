681a0c95d53e37154888c1bd693500705d21f01b
Nick Spagnola <spagnola8@gmail.com>
2019-07-11 09:57:56 -0700

    add regular test page

---
+++ source/ShellPlayground/Views/RegularContentPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ShellPlayground
+{
+    public partial class RegularContentPage : ContentPage
+    {
+        public RegularContentPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
